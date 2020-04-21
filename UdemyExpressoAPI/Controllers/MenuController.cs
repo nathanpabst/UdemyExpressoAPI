9aa190b64e954e1dd38f42865b43e31c61ee6d99
Nathan Pabst <Nathan.Pabst@ey.com>
2020-04-21 16:22:39 -0500

    Add project files.

---
+++ UdemyExpressoAPI/Controllers/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyExpressoAPI.Data;
+
+namespace UdemyExpressoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        // instantiate the ExpressoDbContext object so we can access the data from the menus table
+        ExpressoDbContext _expressoDbContext;
+
+        public MenuController(ExpressoDbContext expressoDbContext)
+        {
+            _expressoDbContext = expressoDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetMenus()
+        {
+            var menus = _expressoDbContext.Menus;
+            return Ok(menus);
+
+        }
+    }
+}
